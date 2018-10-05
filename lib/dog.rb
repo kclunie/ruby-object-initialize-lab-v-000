4b0727fc44d9c894f38b271cac47e1029036e717
kcluniesa <kcluniesa@yahoo.com>
2018-10-05 13:03:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def initialize(breed)
-    @breed = breed
+  def initialize(name)
+    @name = name
   end
  
-  def breed=(breed)
-    @breed = breed
+  def name=(name)
+    @name = name
   end
  
-  def breed
-    @breed
+  def name
+    @name
   end 
   
   def initialize(breed)
